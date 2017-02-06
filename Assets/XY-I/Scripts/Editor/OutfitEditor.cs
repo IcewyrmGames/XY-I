9adc48e4ab35e64dbddceba542815ec6ef93ed48
Justin <booljayj@gmail.com>
2017-02-06 22:11:18 +0100

    Create custom editor for Outfits

---
+++ Assets/XY-I/Scripts/Editor/OutfitEditor.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomEditor( typeof( Outfit ) )]
+public class OutfitEditor : Editor
+{
+	static float FoldedElementHeight = EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing * 2f;
+	static float UnfoldedElementHeight = EditorGUIUtility.singleLineHeight*3f + EditorGUIUtility.standardVerticalSpacing*4f;
+
+	static bool bodyOverrideArrayUnfolded = false;
+	static bool decalOverrideArrayUnfolded = false;
+
+	ReorderableList bodyOverrideArray;
+	ReorderableList decalOverrideArray;
+
+	void OnEnable()
+	{
+		bodyOverrideArray = new ReorderableList(
+			serializedObject,
+			serializedObject.FindProperty( "bodyOverrides" ),
+			true, true, true, true
+		);
+
+		decalOverrideArray = new ReorderableList(
+			serializedObject,
+			serializedObject.FindProperty( "decalOverrides" ),
+			true, true, true, true
+		);
+
+		bodyOverrideArray.elementHeightCallback = (int index) =>
+		{
+			return bodyOverrideArrayUnfolded ? UnfoldedElementHeight : FoldedElementHeight;
+		};
+		decalOverrideArray.elementHeightCallback = (int index) =>
+		{
+			return decalOverrideArrayUnfolded ? UnfoldedElementHeight : FoldedElementHeight;
+		};
+
+		bodyOverrideArray.drawElementCallback = ( Rect rect, int index, bool isActive, bool isFocused ) =>
+		{
+			SerializedProperty property = bodyOverrideArray.serializedProperty.GetArrayElementAtIndex( index );
+			if( !bodyOverrideArrayUnfolded )
+			{
+				rect.y += 2f; rect.height = EditorGUIUtility.singleLineHeight;
+				EditorGUI.PropertyField( rect, property.FindPropertyRelative( "slot" ), GUIContent.none );
+			}
+			else
+			{
+				EditorGUI.PropertyField( rect, property, GUIContent.none );
+			}
+		};
+		decalOverrideArray.drawElementCallback = ( Rect rect, int index, bool isActive, bool isFocused ) =>
+		{
+			SerializedProperty property = decalOverrideArray.serializedProperty.GetArrayElementAtIndex( index );
+			if( !decalOverrideArrayUnfolded )
+			{
+				rect.y += 2f; rect.height = EditorGUIUtility.singleLineHeight;
+				EditorGUI.PropertyField( rect, property.FindPropertyRelative( "slot" ), GUIContent.none );
+			}
+			else
+			{
+				EditorGUI.PropertyField( rect, property, GUIContent.none );
+			}
+		};
+
+		bodyOverrideArray.drawHeaderCallback = (Rect rect) =>
+		{
+			rect.x += 10f;
+			bodyOverrideArrayUnfolded = EditorGUI.Foldout( rect, bodyOverrideArrayUnfolded, "Body Part Overrides", true );
+		};
+		decalOverrideArray.drawHeaderCallback = (Rect rect) =>
+		{
+			rect.x += 10f;
+			decalOverrideArrayUnfolded = EditorGUI.Foldout( rect, decalOverrideArrayUnfolded, "Decal Overrides", true );
+		};
+	}
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+
+		using( var scope = new EditorGUI.DisabledGroupScope( EditorApplication.isPlayingOrWillChangePlaymode ) )
+		{
+			EditorGUILayout.Separator();
+			bodyOverrideArray.DoLayoutList();
+
+			EditorGUILayout.Separator();
+			decalOverrideArray.DoLayoutList();
+		}
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
---
+++ Assets/XY-I/Scripts/Editor/SlotDataDrawer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer( typeof( BodySlotData ) )]
+[CustomPropertyDrawer( typeof( DecalSlotData ) )]
+public class SlotDataDrawer : PropertyDrawer
+{
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+	{
+		return ( EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing ) * 3f;
+	}
+
+	public override void OnGUI( Rect position, SerializedProperty property, GUIContent label )
+	{
+		SerializedProperty slot = property.FindPropertyRelative( "slot" );
+		SerializedProperty sprite = property.FindPropertyRelative( "sprite" );
+		SerializedProperty color = property.FindPropertyRelative( "color" );
+
+		position = EditorGUI.IndentedRect( position );
+		int previousIndent = EditorGUI.indentLevel;
+		EditorGUI.indentLevel = 0;
+		EditorGUIUtility.labelWidth *= 2f/3f;
+
+		position.y += 2f;
+		position.height = EditorGUIUtility.singleLineHeight;
+
+		Rect pos0 = new Rect(
+			position.x,
+			position.y,
+			position.width,
+			position.height
+		);
+		Rect pos1 = new Rect(
+			position.x,
+			pos0.y + pos0.height + EditorGUIUtility.standardVerticalSpacing,
+			position.width,
+			position.height
+		);
+		Rect pos2 = new Rect(
+			position.x,
+			pos1.y + pos1.height + EditorGUIUtility.standardVerticalSpacing,
+			position.width,
+			position.height
+		);
+
+		EditorGUI.PropertyField( pos0, slot, GUIContent.none );
+		EditorGUI.PropertyField( pos1, sprite );
+		EditorGUI.PropertyField( pos2, color );
+
+		EditorGUI.indentLevel = previousIndent;
+	}
+}
